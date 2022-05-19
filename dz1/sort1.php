b59209b5bafe286b0b2fb292f077bf84bcbf79c6
Sergey <pariss8@mail.ru>
2022-05-19 11:54:26 +0300

    Сортировка

---
+++ dz1/sort1.php
@@ -0,0 +1,80 @@
+<?php
+
+// 1) У вас есть массив. Необходимо отсортировать его по PRICE
+
+$array = [
+    'SKLAD1' => [
+        'EDA' => [
+            'TOVAR1' => [
+                'NAME' => '....',
+                'PRICE' => 12343
+            ],
+            'TOVAR2' => [
+                'NAME' => '....',
+                'PRICE' => 12344
+            ],
+        ],
+        'NAPITKI' => [
+            'TOVAR55' => [
+                'NAME' => '....',
+                'PRICE' => 12346
+            ],
+            'TOVAR54' => [
+                'NAME' => '....',
+                'PRICE' => 12345
+            ],
+        ],
+    ],
+
+    'SKLAD2' => [
+        'EDA' => [
+            'TOVAR66' => [
+                'NAME' => '....',
+                'PRICE' => 12347
+            ],
+            'TOVAR67' => [
+                'NAME' => '....',
+                'PRICE' => 12341
+            ],
+        ],
+        'NAPITKI' => [
+            'TOVAR77' => [
+                'NAME' => '....',
+                'PRICE' => 12340
+            ],
+            'TOVAR78' => [
+                'NAME' => '....',
+                'PRICE' => 12342
+            ],
+        ],
+    ],
+];
+
+
+function cmp($a, $b, $field = 'PRICE')
+{
+    if ($a[$field] == $b[$field]) {
+        return 0;
+    }
+    return ($a[$field] < $b[$field]) ? -1 : 1;
+}
+
+
+
+function mysort($a)
+{
+    $new_a = [];
+
+    foreach ($a as $key1 => $val1){
+        foreach ($val1 as $key2 => $val2){
+            $tmp = $val2;
+            uasort($tmp, 'cmp');
+            $new_a[$key1][$key2] = $tmp;
+        }
+    }
+
+    return $new_a;
+}
+
+
+echo '<pre>'; print_r(mysort($array)); echo '</pre>';
